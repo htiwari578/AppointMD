c88c23da004ce23778c702aae6252a0c03b2e179
Himanshu Tiwari <htiwari578@gmail.com>
2024-12-24 17:55:57 +0530

    updated code

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URL);
         console.log("Database connected");
     } catch (error) {
         console.log("Database not connected")
