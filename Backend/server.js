798d5aa4ea56e63eb8d87eed7e2a315239458edb
Himanshu Tiwari <htiwari578@gmail.com>
2024-12-24 14:11:49 +0530

    backend file

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import connectDB from './config/db.js';
+
+
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, ()=>{
+    connectDB();
+    console.log(`Server is running on port ${PORT}`);
+})
