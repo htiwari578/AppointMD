70a64e13d643507d2f6e24904ce2d86e977eb5be
Himanshu Tiwari <htiwari578@gmail.com>
2025-01-24 12:31:38 +0530

    backend code for doc dashboard

--- Backend/controllers/doctor.js
+++ Backend/controllers/doctor.js
@@ -134,3 +134,44 @@ export const appointmentCancel = async (req,res)=>{
         })
     }
 }
+
+//api to get dashboard data for doctor pannel
+
+export const doctorDashboard = async(req,res)=>{
+    try {
+        const {docId} = req.body
+        const appointments = await appointmentModel.find({docId})
+
+        let earnings = 0
+        //totl earnings
+        appointments.map((item)=>{
+            if(item.isCompleted || item.payment){
+                earnings += item.amount
+            }
+        })
+        //total patients
+        let patients = []
+        appointments.map((item)=> {
+            if(!patients.includes(item.userId)){
+                patients.push(item.userId)
+            }
+        })
+
+        const dashData = {
+            earnings,
+            appointments: appointments.length,
+            patients: patients.length,
+            latestAppointments: appointments.reverse().slice(0,5)
+
+        }
+
+        res.json({success:true,dashData})
+
+    } catch (error) {
+        console.log(error);
+        res.json({
+            message:"Server error",
+            success:false
+        })
+    }
+}
--- Backend/routes/doctorRoute.js
+++ Backend/routes/doctorRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { appointmentCancel, appointmentComplete, appointmentsDoctor, doctorList, loginDoctor } from '../controllers/doctor.js'
+import { appointmentCancel, appointmentComplete, appointmentsDoctor, doctorDashboard, doctorList, loginDoctor } from '../controllers/doctor.js'
 import { authDoctor } from '../middleware/authDoctor.js'
 
 const doctorRouter = express.Router()
@@ -9,5 +9,7 @@ doctorRouter.post('/login', loginDoctor)
 doctorRouter.get('/appointments',authDoctor, appointmentsDoctor)
 doctorRouter.post('/complete-appointment',authDoctor, appointmentComplete)
 doctorRouter.post('/cancel-appointment',authDoctor, appointmentCancel)
+doctorRouter.post('/cancel-appointment',authDoctor, appointmentCancel)
+doctorRouter.get('/dashboard',authDoctor, doctorDashboard)
 
 export default doctorRouter;
